1f9639484c804e460bbceee67977fa6c549e41d9
Nikhil Purnam Sheth <npsheth@unix4.lt.ucsc.edu>
2019-04-10 01:17:21 -0700

    added command line arguments + -D option

--- asg1/main.cpp
+++ asg1/main.cpp
@@ -13,41 +13,33 @@ using namespace std;
 #include "auxlib.h"
 #include "string_set.h"
 
-const string cpp_name = "/usr/bin/cpp";
+const string CPP = "/usr/bin/cpp -nostdinc";
 string cpp_command;
+string dOpt = "";
 
-// Open a pipe from the C preprocessor.
-// Exit failure if can't.
-// Assigns opened pipe to FILE* yyin.
-void cpp_popen (const char* filename) {
-   cpp_command = cpp_name + " " + filename;
-   yyin = popen (cpp_command.c_str(), "r");
-   if (yyin == nullptr) {
-      syserrprintf (cpp_command.c_str());
-      exit (exec::exit_status);
-   }else {
-      if (yy_flex_debug) {
-         fprintf (stderr, "-- popen (%s), fileno(yyin) = %d\n",
-                  cpp_command.c_str(), fileno (yyin));
-      }
-      lexer::newfilename (cpp_command);
-   }
+void cpp_popen(const char* filename){
+   string command = CPP + dOpt.c_str() + " " + filename;
+   //printf("%s\n",command.c_str());
 }
+
 
 void scan_opts (int argc, char** argv) {
    opterr = 0;
+   //atm do not need these
    // yy_flex_debug = 0;
    // yydebug = 0;
    // lexer::interactive = isatty (fileno (stdin))
    //                  and isatty (fileno (stdout));
    for(;;) {
-      int opt = getopt (argc, argv, "@:lyD");
+      int opt = getopt (argc, argv, "@:lyD:");
       if (opt == EOF) break;
       switch (opt) {
          case '@': set_debugflags (optarg);   break;
-         case 'D': printf("hi"); break;   //pass to precompiler
+         case 'D': dOpt = dOpt + string(" -D ") + optarg; break;   //pass to precompiler
          // case 'l': yy_flex_debug = 1;         break;
          // case 'y': yydebug = 1;               break;
+         case 'l': printf("l");               break;
+         case 'y': printf("y");               break;
          default:  errprintf ("bad option (%c)\n", optopt); break;
       }
    }
@@ -56,8 +48,7 @@ void scan_opts (int argc, char** argv) {
                  exec::execname.c_str());
       exit (exec::exit_status);
    }
-   const char* filename = optind == argc ? "-" : argv[optind];
-   //cpp_popen (filename);
+   cpp_popen(optind == argc ? "-" : argv[optind]);
 }
 
 
