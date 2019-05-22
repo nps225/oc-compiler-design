e3900680464d6efe9ad0a6e9894e2022de0d73b5
Nikhil Sheth <nps225@gmail.com>
2019-05-22 15:42:07 -0700

    added some starter stuff

---
+++ asg4/symbol.h
@@ -0,0 +1,6 @@
+#ifndef _SYMBOL_H__
+#define _SYMBOL_H__
+
+struct symbol;
+
+#endif
--- asg4/typeChecker.cpp
+++ asg4/typeChecker.cpp
@@ -28,23 +28,3 @@ void post_order(astree* tree){
                tree->symbol,parser::get_tname (tree->symbol), 
                   tree->lexinfo->c_str());
 }
-
-
-// void astree::print (FILE* outfile, astree* tree, int depth) {
-//    //fprintf (outfile, "; %*s", depth * 3, "");
-//    // fprintf (outfile, "%s \"%s\" (%zd.%zd.%zd)\n",
-//    //          parser::get_tname (tree->symbol), tree->lexinfo->c_str(),
-//    //          tree->lloc.filenr, tree->lloc.linenr, tree->lloc.offset);
-
-//    // out = outfile;
- 
-//    fprintf(outfile, "  %1zd    %2zd.%.3zd    %3d    %-13s   %s\n", 
-//                tree->lloc.filenr, tree->lloc.linenr, tree->lloc.offset,
-//                tree->symbol,parser::get_tname (tree->symbol), 
-//                   tree->lexinfo->c_str());
-//    // fprintf(outfile, "   %zd  ", tree->lloc.filenr);
-//    //  fprintf(outfile, "\n");
-   
-//    for (astree* child: tree->children) {
-//       astree::print (outfile, child, depth + 1);
-//    }
