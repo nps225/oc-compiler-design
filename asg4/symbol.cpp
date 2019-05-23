3e1e1bc5121c9a7bb945fd9e644803eb68e0d87f
Nikhil Sheth <nps225@gmail.com>
2019-05-22 23:09:20 -0700

    something

--- asg4/symbol.cpp
+++ asg4/symbol.cpp
@@ -10,22 +10,31 @@
 #include <string.h>
 #include <wait.h>
 #include <bitset>
+#include <unordered_map>
 // Include our own libraries
 #include "string_set.h"
 #include "auxlib.h"
 #include "lyutils.h"
 #include "asg3.h"
-#include "typeChecker.h"
+// #include "typeChecker.h"
 
 
 enum class attr {
-VOID, INT, NULLPTR_T, STRING, STRUCT, ARRAY, FUNCTION, VARIABLE,
-FIELD, TYPEID, PARAM, LOCAL, LVAL, CONST, VREG, VADDR, BITSET_SIZE,
+    VOID, INT, NULLPTR_T, STRING, STRUCT, ARRAY, FUNCTION, VARIABLE,
+    FIELD, TYPEID, PARAM, LOCAL, LVAL, CONST, VREG, VADDR, BITSET_SIZE,
 };
 
+struct symbol;
+
 using attr_bitset = bitset<size_t(attr::BITSET_SIZE)>;
-
-
+using symbol_table = unordered_map<string*,symbol*>;
+using symbol_entry = symbol_table::value_type;
+// symbol table
 struct symbol{
-
+    attr_bitset attributes;
+    size_t sequence = 0; 
+    symbol_table* fields = nullptr;
+    location lloc;
+    size_t block_nr = 0;
+    vector<symbol*>* parameters = nullptr;
 };
