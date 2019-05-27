7cb35b9b38da71fc07e92edc115839f45fb3cca4
Nikhil Sheth <nps225@gmail.com>
2019-05-26 23:44:51 -0700

    added beginning of iterating through functions

--- asg4/typeChecker.cpp
+++ asg4/typeChecker.cpp
@@ -19,16 +19,17 @@
 
 symbol_table* global;
 symbol_table* local;
+void traverse_function(astree* node);
 
 void post_order(astree* tree){
   for (astree* child: tree->children) {
-      fprintf(stdout, "  %1zd    %2zd.%.3zd    %3d    %-13s   %s\n", 
-               child->lloc.filenr, child->lloc.linenr, child->lloc.offset,
-               child->symbol,parser::get_tname (child->symbol), 
-                  child->lexinfo->c_str());
+      // fprintf(stdout, "  %1zd    %2zd.%.3zd    %3d    %-13s   %s\n", 
+      //          child->lloc.filenr, child->lloc.linenr, child->lloc.offset,
+      //          child->symbol,parser::get_tname (child->symbol), 
+      //             child->lexinfo->c_str());
       switch(child->symbol){
         case FUNCTION://handles functions
-             fprintf(stdout,"hello\n");
+             traverse_function(child);
         break;
         case TOK_STRUCT://handles structs
              fprintf(stdout,"hello\n");
@@ -43,5 +44,47 @@ void post_order(astree* tree){
   }
     
 }
+
+
+void traverse_function(astree* node){
+    fprintf(stdout,"%s\n",parser::get_tname(node->symbol));
+    // fprintf(stdout,"%d\n",node->children.size()); 
+    //prototype = 2 children; functions = 3 children
+    if(node->children.size() == 3){
+      int i = 0;
+      for(astree* child: node->children){
+        switch (i){
+          case 0://TYPEID
+          fprintf(stdout,"%s\n",parser::get_tname (child->symbol));
+          i++;
+          break;
+          case 1://TOK_PARAM
+          fprintf(stdout,"%s\n",parser::get_tname (child->symbol));
+          i++;
+          break;
+          case 2://TOK_BLOCK
+          fprintf(stdout,"%s\n",parser::get_tname (child->symbol));
+          i++;
+          break;
+        }
+      }
+    }else{
+      //prototypes
+      int i = 0;
+      for(astree* child: node->children){
+        switch (i){
+          case 0://TYPEID
+          fprintf(stdout,"%s\n",parser::get_tname (child->symbol));
+          i++;
+          break;
+          case 1://TOK_PARAM
+          fprintf(stdout,"%s\n",parser::get_tname (child->symbol));
+          i++;
+          break;
+        }
+      }
+    }
+}
+
 
 
