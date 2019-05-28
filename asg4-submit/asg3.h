acb5b062705c6697e8a0177f308fcd48b73c123f
Nikhil Punathil <nikhilpe@gmail.com>
2019-05-28 01:26:39 -0700

    asg4: add submit directory and remove debug
    Signed-off-by: Nikhil Punathil <nikhilpe@gmail.com>

---
+++ asg4-submit/asg3.h
@@ -0,0 +1,15 @@
+/*
+ *  *  *  oc-compiler asg3.h
+ *   *   *  CMPS 104A Assignment 3
+ *    *    *  Author:
+ *     *     *  Nikhil Punathil <npunathi@ucsc.edu> [SID:1584204]
+ *      *      *  Nikhil Sheth <npsheth@ucsc.edu> [SID:1584204]
+ *       *      Date: 2019.04.29
+ *        */
+
+#ifndef _ASG3_H__
+#define _ASG3_H__
+
+void printLexer(FILE* out);
+
+#endif
