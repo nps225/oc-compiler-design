92c1d719cabcb0ddcdfc742bbf23dfe630eefa8d
nps225 <nps225@gmail.com>
2019-06-01 20:38:27 -0700

    structs finished?

--- asg5/emitter.cpp
+++ asg5/emitter.cpp
@@ -18,7 +18,7 @@
 
 
 string output = "";
-
+int string_globals = 0;
 void emit_the_tree(astree* node){
 
     for(astree* child: node->children){
@@ -33,7 +33,7 @@ void emit_the_tree(astree* node){
                break;
            }
            case TYPE_ID:{
-            //    produce_type_id_output(child);
+               produce_type_id_output(child);
                break;
            }
        }
@@ -49,12 +49,69 @@ void produce_function_output(astree* child){
 
 void produce_struct_output(astree* child){
     //print he head
-    output += ".struct";
-    printf("%s",child->children.at(0)->lexinfo.c_str());
-    //we need to parse the body
-    // fprintf(stdout,"",);
+    output += ".struct ";
+    // printf("%s\n",*(child->children.at(0)->lexinfo));
+    output += *(child->children.at(0)->lexinfo) + '\n';
+    //the second child is a block
+    produce_struct_block_output(child->children.at(1));
+    output += ".end \n";
 }
 
-void produce_type_id_output(astree* child){
+void produce_struct_block_output(astree* node){
+    for(astree* child: node->children){
+        produce_type_size_output(child);
+    }
+}
+
+void produce_type_size_output(astree* node){
+     output += ".field ";
+     switch(node->children.at(0)->symbol){
+         case TOK_PTR:{
+            output +="ptr ";
+            output +=*(node->children.at(1)->lexinfo);
+            break;
+         }
+         case TOK_INT:{
+             output +="int ";
+             output += *(node->children.at(1)->lexinfo);
+             break;
+         }
+         case TOK_CHAR:{
+             output +="char ";
+             output += *(node->children.at(1)->lexinfo);
+             break;
+         }
+         case TOK_STRING:{
+             output +="string ";
+             output += *(node->children.at(1)->lexinfo);
+             break;
+         }
+              
+     }
+     output += '\n';
+}
+
+void produce_type_id_output(astree* node){
     // fprintf(stdout,"",);
+    //there are two types of global variables
+    switch(node->children.at(0)->symbol){
+        case TOK_STRING:{//will hande stringdef
+            //assume always 3 children
+            output = output + ".s" + std::to_string(string_globals) + ":";
+            
+            string_globals++;
+            break;
+        }
+        default:{//for the rest of the cases
+            produce_label(node);
+            break;
+        }
+    }
+    output += "\n";
+}
+
+void produce_label(astree* node){
+     //always look at second child
+     output += *(node->children.at(1)->lexinfo);
+
 }
